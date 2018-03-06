3b54ecfce022b13fcce5e942ff8a440591e92d6c
Sanket Chintal <sanket@247software.in>
2018-03-06 15:43:29 +0530

    wew

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'src/ApiConsumer/Consumer.php';
 //require_once 'vendor/autoload.php'; // if using Composer autoloader and psr-0
 use ApiConsumer\Consumer;
@@ -6,7 +7,7 @@ use ApiConsumer\Consumer;
 
 /**
  * The example below uses the API at Active.com to query "Running" activities in
- * Florida with a start date within the next month that meet the query string of
+ * Florida wefeith a start date within the next month that meet the query string of
  * "ultra marathon". It will return a result set of 25 records as set.
  * 
  * @author Adam Culp http://www.geekyboy.com
